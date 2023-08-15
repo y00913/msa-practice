03d92a437ef2fd3c3e82989519cf0878b5eb10b7
y00913 <ayt0913@gmail.com>
2023-08-16 05:22:51 +0900

    feat: config server & user 연동 완료

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -1,14 +1,11 @@
 package com.example.user;
 
-import com.example.user.config.TestConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @RefreshScope
-@EnableConfigurationProperties(TestConfig.class)
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UserApplication {
--- user/src/main/java/com/example/user/service/UserServiceImpl.java
+++ user/src/main/java/com/example/user/service/UserServiceImpl.java
@@ -1,8 +1,8 @@
 package com.example.user.service;
 
-import com.example.user.config.TestConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +12,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
 
-    private final TestConfig testConfig;
+    @Value("${test}")
+    private String user;
 
     @Override
     public String findUser() {
 
 
-        return testConfig.getTest();
+        return user;
     }
 
 }
