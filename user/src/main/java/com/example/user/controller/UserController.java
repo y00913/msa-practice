d465276d27feeadcf4220dd3f0f45129a07b5cf2
y00913 <ayt0913@gmail.com>
2023-08-18 18:22:20 +0900

    feat: api 추가

--- user/src/main/java/com/example/user/controller/UserController.java
+++ user/src/main/java/com/example/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.user.controller;
 import com.example.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,16 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/test")
+    @GetMapping("/user")
     public String userInfo(){
         return userService.findUser();
+    }
+
+    @PostMapping("/user")
+    public String saveUser() {
+        userService.saveUser();
+
+        return "save user";
     }
 
 }
--- user/src/main/java/com/example/user/service/UserServiceImpl.java
+++ user/src/main/java/com/example/user/service/UserServiceImpl.java
@@ -28,7 +28,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void saveUser() {
-        
+
     }
 
 }
